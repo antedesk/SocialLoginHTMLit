8fa507f022b9cf72b6a3a7579967a6a1346da83e
Antonio Tedeschi <antedesk@gmail.com>
2017-09-28 15:40:10 +0200

    Facebook login

--- app/src/main/java/it/antedesk/socialloginhtmlit/LoginActivity.java
+++ app/src/main/java/it/antedesk/socialloginhtmlit/LoginActivity.java
@@ -3,58 +3,52 @@ package it.antedesk.socialloginhtmlit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
-import com.twitter.sdk.android.core.Callback;
-import com.twitter.sdk.android.core.DefaultLogger;
-import com.twitter.sdk.android.core.Result;
-import com.twitter.sdk.android.core.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.core.TwitterConfig;
-import com.twitter.sdk.android.core.TwitterException;
-import com.twitter.sdk.android.core.TwitterSession;
-import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 
 public class LoginActivity extends AppCompatActivity {
 
-    private TwitterLoginButton twitterLoginButton;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        initializeTwitter();
-        // set contentView must be called after the Twitter initialization
-        //otherwise the SDK will raise an exception!
         setContentView(R.layout.activity_login);
+        callbackManager = CallbackManager.Factory.create();
 
-        twitterLoginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
-        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton fbLoginButton = (LoginButton) findViewById(R.id.fb_login_button);
+        fbLoginButton.setReadPermissions("email");
+
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void success(Result<TwitterSession> result) {
+            public void onSuccess(LoginResult loginResult) {
                 Toast.makeText(getApplicationContext(), "Logged!", Toast.LENGTH_LONG).show();
             }
 
             @Override
-            public void failure(TwitterException exception) {
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "Cancel Event!", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
                 Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    private void initializeTwitter(){
-        TwitterConfig config = new TwitterConfig.Builder(this)
-                .logger(new DefaultLogger(Log.DEBUG))
-                .twitterAuthConfig(new TwitterAuthConfig(getString(R.string.com_twitter_sdk_android_CONSUMER_KEY), getString(R.string.com_twitter_sdk_android_CONSUMER_SECRET)))
-                .debug(true)
-                .build();
-        Twitter.initialize(config);
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        twitterLoginButton.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
+
+
 }
