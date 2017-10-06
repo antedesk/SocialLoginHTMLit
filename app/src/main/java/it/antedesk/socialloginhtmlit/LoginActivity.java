44f84aeeef4f15982cf0be29ac263df8ccef5d18
Antonio Tedeschi <antedesk@gmail.com>
2017-10-06 16:48:20 +0200

    TwitterLogin - aggiunto bottone per il logout.

--- app/src/main/java/it/antedesk/socialloginhtmlit/LoginActivity.java
+++ app/src/main/java/it/antedesk/socialloginhtmlit/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.twitter.sdk.android.core.Callback;
@@ -11,7 +13,9 @@ import com.twitter.sdk.android.core.DefaultLogger;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterAuthToken;
 import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
@@ -19,6 +23,7 @@ import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 public class LoginActivity extends AppCompatActivity {
 
     private TwitterLoginButton twitterLoginButton;
+    private Button twitterLogoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +38,27 @@ public class LoginActivity extends AppCompatActivity {
         twitterLoginButton.setCallback(new Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> result) {
-                Toast.makeText(getApplicationContext(), "Logged!", Toast.LENGTH_LONG).show();
+                updateUI(true);
+                TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+                TwitterAuthToken authToken = session.getAuthToken();
+                Toast.makeText(getApplicationContext(), "Logged! My token is: "+authToken.token, Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void failure(TwitterException exception) {
+                updateUI(false);
                 Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        twitterLogoutButton = (Button) findViewById(R.id.twitter_logout_button);
+        twitterLogoutButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TwitterCore.getInstance().getSessionManager().clearActiveSession();
+                TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+                if (session == null)
+                    Toast.makeText(getApplicationContext(), "Logged out!", Toast.LENGTH_LONG).show();
+                updateUI(false);
             }
         });
     }
@@ -52,9 +72,22 @@ public class LoginActivity extends AppCompatActivity {
         Twitter.initialize(config);
     }
 
+    private void updateUI(boolean signedIn) {
+        if (signedIn) {
+            twitterLoginButton.setVisibility(View.GONE);
+            twitterLogoutButton.setVisibility(View.VISIBLE);
+        } else {
+            twitterLoginButton.setVisibility(View.VISIBLE);
+            twitterLogoutButton.setVisibility(View.GONE);
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         twitterLoginButton.onActivityResult(requestCode, resultCode, data);
     }
+
+
+
 }
